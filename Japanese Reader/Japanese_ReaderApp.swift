59ba0330d3850c6b09f10ae9c703cc9bce8718ba
Hiro Ayettey <hiro.ayettey@gmail.com>
2026-01-06 19:27:46 -0500

    Initial Commit

---
+++ Japanese Reader/Japanese_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Japanese_ReaderApp.swift
+//  Japanese Reader
+//
+//  Created by Hiro Ayettey on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Japanese_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
